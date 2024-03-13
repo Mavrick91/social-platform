133d04354e9f780f97f4271bdeb265787f9d9e13
Mavrick91 <ray.mavrick@gmail.com>
2024-03-13 21:56:09 +0800

    Remove dark mode toggle button from Header component

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -32,29 +32,6 @@ export default function Header() {
             </span>
           </Link>
           <div className="flex items-center gap-4">
-            <button
-              data-tooltip-target="tooltip-dark"
-              type="button"
-              className="inline-flex items-center p-2 mr-1 text-sm font-medium text-gray-500 rounded-lg dark:text-gray-400 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-            >
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-              </svg>
-            </button>
-            <div
-              id="tooltip-dark"
-              role="tooltip"
-              className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700"
-            >
-              Toggle dark mode
-              <div className="tooltip-arrow" data-popper-arrow></div>
-            </div>
-
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <button className="size-10 items-center justify-center flex rounded-full bg-slate-400">
