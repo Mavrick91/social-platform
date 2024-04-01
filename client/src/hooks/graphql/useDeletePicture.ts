15171c45a37bef2f348c155ed4cb797287db35b3
Mavrick91 <ray.mavrick@gmail.com>
2024-04-01 18:05:36 +0800

    Add cache modification and evict logic in useDeletePicture hook

--- client/src/hooks/graphql/useDeletePicture.ts
+++ client/src/hooks/graphql/useDeletePicture.ts
@@ -1,9 +1,20 @@
 import { useDeletePictureMutation } from '@/__generated__/graphql';
+import { updateUserCount } from '@/lib/cacheUtils';
 
 export function useDeletePicture() {
   return useDeletePictureMutation({
     update(cache, { data }) {
       if (data?.deletePicture) {
+        cache.modify({
+          fields: {
+            user(existingUser = {}, { readField }) {
+              const userId: number | undefined = readField('id', existingUser);
+
+              if (userId) updateUserCount(cache, userId, 'pictures', -1);
+              return existingUser;
+            },
+          },
+        });
         cache.evict({ id: cache.identify(data.deletePicture) });
       }
     },
