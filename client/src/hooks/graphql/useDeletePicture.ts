9b27509c930b38839d67e5dbb2b28b88b3e1daf9
Mavrick91 <ray.mavrick@gmail.com>
2024-04-01 18:00:14 +0800

    Add AccessibilityExpand component and update useDeletePicture hook

--- client/src/hooks/graphql/useDeletePicture.ts
+++ client/src/hooks/graphql/useDeletePicture.ts
@@ -1,26 +1,10 @@
 import { useDeletePictureMutation } from '@/__generated__/graphql';
-import { updateUserCount } from '@/lib/cacheUtils';
-import { Reference } from '@apollo/client';
 
 export function useDeletePicture() {
   return useDeletePictureMutation({
     update(cache, { data }) {
       if (data?.deletePicture) {
-        cache.modify({
-          fields: {
-            picturesByAuthor(existingPictures = [], { readField }) {
-              return existingPictures.filter((pictureRef: Reference) => {
-                return data.deletePicture.id !== readField('id', pictureRef);
-              });
-            },
-            user(existingUser = {}, { readField }) {
-              const userId: number | undefined = readField('id', existingUser);
-
-              if (userId) updateUserCount(cache, userId, 'pictures', -1);
-              return existingUser;
-            },
-          },
-        });
+        cache.evict({ id: cache.identify(data.deletePicture) });
       }
     },
   });
