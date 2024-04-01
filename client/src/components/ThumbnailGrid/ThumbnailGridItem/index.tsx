949a0071df2821bef6978c0ec61343662c74bf60
Mavrick91 <ray.mavrick@gmail.com>
2024-04-01 19:22:57 +0800

    Update ThumbnailGridItem component to use full width and height for the image

--- client/src/components/ThumbnailGrid/ThumbnailGridItem/index.tsx
+++ client/src/components/ThumbnailGrid/ThumbnailGridItem/index.tsx
@@ -16,11 +16,9 @@ export default function ThumbnailGridItem({ picture }: Props) {
         >
           <img
             alt={picture.altText}
-            className="aspect-square overflow-hidden border border-slate-200 object-cover"
+            className="w-full h-full overflow-hidden border border-slate-200 object-cover"
             src={picture.sizes.thumbnail}
             loading="lazy"
-            width={300}
-            height={300}
           />
           <div className="hidden z-20 text-white gap-3 absolute inset-0 bg-black bg-opacity-50 group-hover:flex items-center justify-center">
             <MessageCircle fill="white" size={32} />
