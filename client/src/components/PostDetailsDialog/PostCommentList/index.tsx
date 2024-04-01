5a63204e010e7505c084cedf79e9dcb7a4e613a3
Mavrick91 <ray.mavrick@gmail.com>
2024-04-01 18:16:49 +0800

    Add useMemo hook to improve performance and display "No comments yet" message if there are no comments

--- client/src/components/PostDetailsDialog/PostCommentList/index.tsx
+++ client/src/components/PostDetailsDialog/PostCommentList/index.tsx
@@ -6,7 +6,7 @@ import PostCTA from '@/components/PostCTA';
 import PictureCommentForm from '@/components/PostDetailsDialog/PostCommentForm';
 import { useUserInfo } from '@/providers/UserInfoProvider';
 import moment from 'moment';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import PostCommentItem from '../PostCommentItem';
 
 type Props = {
@@ -27,6 +27,12 @@ function PostCommentList({ picture, setErrorMutation }: Props) {
     }
   }, [error, setErrorMutation]);
 
+  const hasComments = useMemo(() => {
+    if (!data) return false;
+
+    return data.commentsByPictureId.length > 0 || picture.description;
+  }, [data, picture.description]);
+
   if (loading) return <div>Loading comments...</div>;
 
   return (
@@ -35,6 +41,13 @@ function PostCommentList({ picture, setErrorMutation }: Props) {
         className="space-y-2 pr-6 p-3 h-0 grow overflow-y-auto"
         ref={commentListRef}
       >
+        {!hasComments && (
+          <div className="flex flex-col items-center justify-center grow h-full">
+            <span className="font-bold text-2xl">No comments yet.</span>
+            <span className="text-sm mt-2">Start the conversation.</span>
+          </div>
+        )}
+
         <PostCommentItem
           avatar={user.avatar}
           content={picture.description}
